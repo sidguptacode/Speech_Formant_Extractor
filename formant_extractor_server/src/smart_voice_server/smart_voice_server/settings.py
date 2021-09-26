095b57bb3cef4862b086f468654c848543370c2d
Sid Gupta <sid.gupta@mail.utoronto.ca>
2021-09-26 12:04:03 -0400

    Small update to server config

--- formant_extractor_server/src/smart_voice_server/smart_voice_server/settings.py
+++ formant_extractor_server/src/smart_voice_server/smart_voice_server/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wq9ta#!1jeri^%sce)(int0u8@)ix^&9@ke(s_(+6so_mn(@uw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
